a7e215153f86253aa50c2a574b7fcdd64a6334a8
2hardcurrency <sinafavo@gmail.com>
2023-06-23 09:50:00 -0700

    tasks

--- 0x0A-configuration_managemnet/0-create_a_file.pp
+++ 0x0A-configuration_managemnet/0-create_a_file.pp
@@ -1,7 +1,10 @@
-ile { '/tmp/school':
-  ensure => file,
-  mode   => '0744',
-  owner  => 'www-data',
-  group  => 'www-data',
+# Create file in /tmp
+
+file { '/tmp/school' :
+  path    => '/tmp/school',
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
   content => 'I love Puppet',
 }
